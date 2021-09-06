6030f73b4bc40f396bd7878406c56f9752f3a7fe
wesley.cardoso <wesley.cardoso@gazin.com.br>
2021-09-06 11:10:48 -0300

    feat: evaluate s-expression

--- parsing.c
+++ parsing.c
@@ -152,50 +152,101 @@ void lval_print(lval* v) {
 
 void lval_println(lval* v) { lval_print(v); putchar('\n'); }
 
+lval* lval_pop(lval* v, int i) {
+  lval* x = v->cell[i];
 
-// lval evaluate_operation(lval x, char* operator, lval y) {
-//   if (strcmp(operator, "+") == 0) { return lval_num(x.num + y.num); }
-//   if (strcmp(operator, "-") == 0) { return lval_num(x.num - y.num); }
-//   if (strcmp(operator, "*") == 0) { return lval_num(x.num * y.num); }
-//   if (strcmp(operator, "%") == 0) { return lval_num(fmod(x.num, y.num)); }
-//   if (strcmp(operator, "^") == 0) { return lval_num(pow(x.num, y.num)); }
-//   if (strcmp(operator, "min") == 0) { return lval_num((x.num < y.num)? x.num : y.num); }
-//   if (strcmp(operator, "max") == 0) { return lval_num((x.num > y.num)? x.num : y.num); }
+  memmove(&v->cell[i], &v->cell[i+1], sizeof(lval*) * (v->count-i-1));
 
-//   if (strcmp(operator, "/") == 0) { 
-//     return y.num == 0
-//       ? lval_err(LERR_DIV_ZERO)
-//       : lval_num(x.num / y.num);
-//   }
+  v->count--;
 
-//   return lval_err(LERR_BAD_OP);
-// }
+  v->cell = realloc(v->cell, sizeof(lval*) * v->count);
 
-// lval evaluate(mpc_ast_t* t) {
+  return x;
+}
 
-//   if (strstr(t->tag, "number")) {
-//     errno = 0;
-//     double x = strtod(t->contents, NULL);
-//     return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
-//   }
-  
-//   char* operator = t->children[1]->contents;
-  
-//   lval x = evaluate(t->children[2]);
+lval* lval_take(lval* v, int i) {
+  lval* x = lval_pop(v, i);
+  lval_del(v);
 
-//   int i = 3;
+  return x;
+}
 
-//   while (strstr(t->children[i]->tag, "expr")) {
-//     x = evaluate_operation(x, operator, evaluate(t->children[i]));
-//     i++;
-//   }
+lval* builtin_op(lval* a, char* op) {
+  for (int i = 0; i < a->count; i++) {
+    if (a->cell[i]->type != LVAL_NUM) {
+      lval_del(a);
+      return lval_err("Cannot operate on non-number!");
+    }
+  }
 
-//   if(t->children_num == 4 && strcmp(operator, "-") == 0) {
-//     return lval_num(-x.num);
-//   }
+  lval* x = lval_pop(a, 0);
 
-//   return x;
-// }
+  if ((strcmp(op, "-") == 0) && a->count == 0) {
+    x->num = -x->num;
+  }
+
+  while (a->count > 0) {
+    lval* y = lval_pop(a, 0);
+
+    if (strcmp(op, "+") == 0) { x->num += y->num; }
+    if (strcmp(op, "-") == 0) { x->num -= y->num; }
+    if (strcmp(op, "*") == 0) { x->num *= y->num; }
+    if (strcmp(op, "%") == 0) { x->num = fmod(x->num, y->num); }
+    if (strcmp(op, "^") == 0) {  x->num = pow(x->num, y->num); }
+    if (strcmp(op, "min") == 0) { x->num = (x->num < y->num)? x->num : y->num; }
+    if (strcmp(op, "max") == 0) { x->num = (x->num > y->num)? x->num : y->num; }
+
+    if (strcmp(op, "/") == 0) { 
+      if (y->num == 0) {
+        lval_del(x); 
+        lval_del(y);
+        x = lval_err("Division By Zero!"); 
+        break;
+      }
+      x->num /= y->num;
+    }
+    lval_del(y);
+  }
+
+  lval_del(a);
+  return x;
+}
+
+lval* lval_eval_sexpr(lval* v);
+
+lval* lval_eval(lval* v) {
+  if (v->type == LVAL_SEXPR) { return lval_eval_sexpr(v); }
+
+  return v;
+}
+
+
+lval* lval_eval_sexpr(lval* v) {
+  for(int i = 0; i < v->count; i++) {
+    v->cell[i] = lval_eval(v->cell[i]);
+  }
+
+  for(int i = 0; i < v->count; i++) {
+    if (v->cell[i]->type == LVAL_ERR) { return lval_take(v, i); }
+  }
+
+  if (v->count == 0) { return v; }
+
+  if (v->count == 1) { return lval_take(v, 0); }
+
+  lval* f = lval_pop(v, 0);
+
+  if (f->type != LVAL_SYM) {
+    lval_del(f); 
+    lval_del(v);
+    return lval_err("S-expression does not start with symbol!");
+  }
+
+  lval* result = builtin_op(v, f->sym);
+  lval_del(f);
+
+  return result;
+}
 
 int main(int argc, char** argv) {
 
@@ -225,9 +276,8 @@ int main(int argc, char** argv) {
 
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
-      puts("entrou1");
-      lval* x = lval_read(r.output);
-      puts("saiu1");
+      
+      lval* x = lval_eval(lval_read(r.output));
       lval_println(x);
       lval_del(x);
 
